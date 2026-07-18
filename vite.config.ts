f6fd345a56f1c60c16ce920f150bf26d757de58c
Zulham Syafrawi <zulham.sy@gmail.com>
2026-07-18 14:43:46 +0700

    fix: build error railway

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
